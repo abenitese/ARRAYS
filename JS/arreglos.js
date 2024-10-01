93399639e232ba4ae1c97b8546ed9a44d9a41c86
Alex Benites E. <66579129+abenitese@users.noreply.github.com>
2024-10-01 11:36:56 -0500

    Add files via upload

---
+++ JS/arreglos.js
@@ -0,0 +1,21 @@
+//let miVector=["alex","miguel","benites","espinoza"];
+//console.log(miVector);
+//console.log(miVector[2]);//obtiene la 3ra posicion: benites
+//let miMatriz=[[1,2,3],[4,5,6],[7,8,9]];//cada corchete es una fila y cada valor una columna
+//console.log(miMatriz);
+//console.log(miMatriz[1][1]);//obtiene el valor del medio: 5
+
+let vector = new Array(3);
+vector[0]="alex";
+vector[1]="miguel";
+vector[2]="benites";
+console.log(vector);
+
+//matriz de 3x3
+let matriz=new Array(3);//crea 3 filas
+
+for (let i=0;i<3;i++) {
+    matriz[i]=new Array(3);//crea 3 columnas
+}
+
+console.log(matriz);
